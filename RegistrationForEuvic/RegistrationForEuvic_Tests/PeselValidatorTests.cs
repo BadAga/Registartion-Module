b7ac5df80ab7004ded7cc7bff8be545e148490bd
BadAga <agnieszka.badocha@gmail.com>
2023-03-20 18:52:15 +0100

    feat: unit testing for validators

---
+++ RegistrationForEuvic/RegistrationForEuvic_Tests/PeselValidatorTests.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RegistrationForEuvic.Models.Validators;
+
+namespace RegistrationForEuvic_Test
+{
+    public class PeselValidatorTests
+    {
+        [Fact]
+        public void PeselValid_WrongLength_LengthShouldBe11()
+        {
+            PeselValid peselValid = new PeselValid();
+
+            string testedPesel1= "1234567891655656565";
+            string testedPesel2 = "123456";
+            string testedPesel3 = "68082095213";
+
+            bool result = peselValid.IsValid(testedPesel1);
+            bool result2 = peselValid.IsValid(testedPesel2);
+            bool result3 = peselValid.IsValid(testedPesel3);
+
+            Assert.False(result);
+            Assert.False(result2);
+            Assert.True(result3);
+        }
+
+        [Fact]
+        public void PeselValid_WrongCharacters_CharacterstMustBeOnlyDigits()
+        {
+            PeselValid peselValid = new PeselValid();
+
+            string testedPesel1 = "123456k7896";
+            string testedPesel2 = "126  6a7891";
+            string testedPesel3 = "123/?6a7891";
+            string testedPesel4 = "85120449896";
+
+            bool result = peselValid.IsValid(testedPesel1);
+            bool result2 = peselValid.IsValid(testedPesel2);
+            bool result3= peselValid.IsValid(testedPesel3);
+            bool result4 = peselValid.IsValid(testedPesel4);
+
+            Assert.False(result);
+            Assert.False(result2);
+            Assert.False(result3);
+            Assert.True(result4);
+        }
+
+        [Fact]
+        public void PeselValid_InocrrectControlNumber_ShouldReturnFalse()
+        {
+            PeselValid peselValid = new PeselValid();
+            string testedPesel1 = "85091029933";
+            string testedPesel2 = "99031769287";
+            string testedPesel3 = "49080193781";
+            string testedPesel4 = "72040747634";
+
+            bool result = peselValid.IsValid(testedPesel1);
+            bool result2 = peselValid.IsValid(testedPesel2);
+            bool result3 = peselValid.IsValid(testedPesel3);
+            bool result4 = peselValid.IsValid(testedPesel4);
+
+            Assert.False(result);
+            Assert.False(result2);
+            Assert.False(result3);
+            Assert.True(result4);
+        }
+
+        [Fact]
+        public void PeselValid_ErrorMessages_ShouldBeAdequate()
+        {
+            PeselValid peselValid = new PeselValid();
+            string testedPesel1 = "85091029933";
+            string testedPesel2 = "123/?6a7891";
+            string testedPesel3 = "123456";
+
+            ValidationContext validationContext = new ValidationContext(testedPesel1);
+            ValidationResult validationResult = peselValid.GetValidationResult(testedPesel1,validationContext);
+
+            Assert.Equal("Incorrect PESEL", validationResult.ErrorMessage);
+ 
+
+            validationContext = new ValidationContext(testedPesel2);
+            validationResult = peselValid.GetValidationResult(testedPesel2, validationContext);
+            Assert.Equal("PESEL contains only numbers", validationResult.ErrorMessage);
+
+
+            validationContext = new ValidationContext(testedPesel3);
+            validationResult = peselValid.GetValidationResult(testedPesel3, validationContext);
+            Assert.Equal("Incorrect PESEL length", validationResult.ErrorMessage);
+        }
+    }
+}
---
+++ RegistrationForEuvic/RegistrationForEuvic_Tests/PhoneValidatorTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using RegistrationForEuvic.Models.Validators;
+
+namespace RegistrationForEuvic_Test
+{
+    public class PhoneValidatorTest
+    {
+        [Fact]
+        public void PhoneValid_IncorrectNumberOfDigits_MustBeOnly9()
+        {
+            PhoneValid phoneValidator = new PhoneValid();
+            bool[] testCasesResults = {phoneValidator.IsValid("12345667899"),
+                                phoneValidator.IsValid("123 456 7899"),
+                                phoneValidator.IsValid("123 456 55"),
+
+                                phoneValidator.IsValid("123 456 556"),
+                                phoneValidator.IsValid("12 456 55 62"),
+                                phoneValidator.IsValid("123-456-556"),
+                                phoneValidator.IsValid("12-456-55-62"),
+                                phoneValidator.IsValid("666456963")};
+
+            bool[] expectedResults = {false,false,false,
+                                      true,true,true,true,true};
+
+            Assert.Equal(expectedResults, testCasesResults);
+        }
+
+        [Fact]
+        public void PhoneValid_CharacterValue_ShouldOnlyBeDigitsWithOptionalSeperator()
+        {
+            PhoneValid phoneValidator =new PhoneValid();
+
+            bool[] testCasesResults = {phoneValidator.IsValid("12345698k"),
+                                phoneValidator.IsValid("12 345 69 8k"),
+                                phoneValidator.IsValid("123 456 98k"),
+                                phoneValidator.IsValid("12-345-69-8k"),
+                                phoneValidator.IsValid("123-456-98k"),
+
+                                phoneValidator.IsValid("123456789"),
+                                phoneValidator.IsValid("123 456 789")};
+
+            bool[] expectedResults = {false,false,false,false,false,
+                                      true,true};
+
+            Assert.Equal(expectedResults, testCasesResults);
+        }
+
+        [Fact]
+        public void PhoneValid_SeparatrValue_OnlySpaceOrDash()
+        {
+            PhoneValid phoneValidator = new PhoneValid();
+
+            bool[] testCasesResults = {phoneValidator.IsValid("123.123.122"),
+                                    phoneValidator.IsValid("123/123/122"),
+                                    phoneValidator.IsValid("123  123  122"),
+                                    phoneValidator.IsValid("123/123/122"),
+                                    phoneValidator.IsValid("12.123.56.96"),
+                                    phoneValidator.IsValid("12/123/56/96"),
+                                    phoneValidator.IsValid("12  123  56  96"),
+                                    phoneValidator.IsValid("12,123,56,96"),
+
+                                    phoneValidator.IsValid("123 123 122"),
+                                    phoneValidator.IsValid("123-123-122"),
+                                    phoneValidator.IsValid("12 123 56 96"),
+                                    phoneValidator.IsValid("12-123-56-96") };
+
+            bool[] expectedResults = {false,false,false,false,false,false,false,false,
+                                      true,true,true,true};
+
+            Assert.Equal(expectedResults, testCasesResults);
+        }
+
+        [Fact]
+        public void PhoneValid_CorrecttFormat()
+        {
+            PhoneValid phoneValidator = new PhoneValid();
+
+            bool[] testCasesResults = {phoneValidator.IsValid("1 2 3 4 5 6 7 8 9"),
+                                    phoneValidator.IsValid("12 34 56 78 9"),
+                                    phoneValidator.IsValid("1 23 45 67 89"),
+                                    phoneValidator.IsValid("1234 56789"),
+                                    phoneValidator.IsValid("12345 6789"),
+                                    phoneValidator.IsValid("123456 789"),
+                                    phoneValidator.IsValid("1234567 89"),
+                                    phoneValidator.IsValid("12345678 9"),
+                                    phoneValidator.IsValid("12 34 567 89"),
+                                    phoneValidator.IsValid("123 45 67 89"),
+                                    phoneValidator.IsValid("12 34 56 899"),
+
+                                    phoneValidator.IsValid("123 123 122"),
+                                    phoneValidator.IsValid("123-123-122"),
+                                    phoneValidator.IsValid("12 123 56 96"),
+                                    phoneValidator.IsValid("12-123-56-96") };
+
+            bool[] expectedResults = {false,false,false,false,false,false,false,false,false,false,false,
+                                      true,true,true,true};
+
+            Assert.Equal(expectedResults, testCasesResults);
+        }
+    }
+}
