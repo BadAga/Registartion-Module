5137c7954ce409f7295a0d08e5cfa4bda996884a
BadAga <agnieszka.badocha@gmail.com>
2023-03-31 12:07:48 +0200

    fix: no unmentioned methods

--- RegistrationForEuvic/RegistrationForEuvic/Controllers/UsersController.cs
+++ RegistrationForEuvic/RegistrationForEuvic/Controllers/UsersController.cs
@@ -66,35 +66,6 @@ namespace RegistrationForEuvic.Controllers
             return user;
         }
 
-        // PUT: api/Users/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
-        {
-            if (id != user.UserId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
 
         // POST: api/Users/Register
         [HttpPost]
@@ -169,21 +140,6 @@ namespace RegistrationForEuvic.Controllers
 
         }
 
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
 
         /// <summary>
         /// Checks if user with passed id already exists in database
